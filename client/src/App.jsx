0933361563070b31a3b414f1bc941a83e91a841e
Hardik Sisodiya <hardik04062003@gmail.com>
2025-08-24 18:53:59 +0530

    project init

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-red-300">
+      <Button className={"bg-red-300"}>hello</Button>
+    </div>
+  );
+};
+
+export default App;
